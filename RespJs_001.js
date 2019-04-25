44714f723a485bafebdb635328b8b12b08418cdd
Geovanearaujo <43480433+Geovanearaujo@users.noreply.github.com>
2019-04-25 11:18:14 -0300

    ResJs_001.js

---
+++ RespJs_001.js
@@ -0,0 +1,8 @@
+function Q2() {
+   console.log('Q2');
+   var x = Number.parseFloat(document.getElementById('x').value);
+   var y = Number.parseFloat(document.getElementById('y').value);
+   // Segue aqui seu código
+}   
+
+<button type="button" onclick="Q2()">Resposta Q2</button>
